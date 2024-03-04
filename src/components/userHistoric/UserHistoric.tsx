bc88fd4795dfe2f745122ea6e9b357ec833264e5
GabrielTFernandes <devgabrieltrigueiro@gmail.com>
2024-03-04 20:14:22 -0300

    home scrolando histórica dinâmico

--- src/components/userHistoric/UserHistoric.tsx
+++ src/components/userHistoric/UserHistoric.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import { ScrollView, StatusBar, View } from "react-native";
+import React, { useRef } from "react";
+import { Animated, Button, ScrollView } from "react-native";
 import { Container, ListTitle, MainTitle, SecondaryTitle } from "./styles";
+import { List } from "react-native-paper";
 
 const UserHistoric = () => {
+  const scrollY = useRef(new Animated.Value(0)).current;
+
   const handleScroll = (event: any) => {
     const offsetY = event.nativeEvent.contentOffset.y;
     if (offsetY <= 0) {
@@ -16,52 +19,10 @@ const UserHistoric = () => {
         <MainTitle>Transações</MainTitle>
         <SecondaryTitle>ver todas</SecondaryTitle>
       </ListTitle>
-      <ScrollView onScroll={handleScroll}>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
-        <SecondaryTitle>ver todas</SecondaryTitle>
+      <ScrollView onScroll={handleScroll} stickyHeaderIndices={[1]}>
+        {Array.from({ length: 20 }).map((_, index) => (
+          <List.Item key={index} title={`Item ${index + 1}`} />
+        ))}
       </ScrollView>
     </Container>
   );
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { ScrollView } from "react-native";
-import { useTheme } from "styled-components";
+import React, { useState } from "react";
+import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import { Body, Container, Header } from "./styles";
 import UserHeader from "../../components/userHeader/UserHeader";
 import HomeCard from "../../components/homeCard/HomeCard";
 import FinancialInfos from "../../components/financialInfos/FinancialInfos";
-import UserHistoric from "../../components/userHistoric/UserHistoric";
+import { List } from "react-native-paper";
 
 const Home = () => {
-  const theme = useTheme();
+  const [full, setFull] = useState(false);
+
+  const handleScroll = (event: any) => {
+    const { contentOffset } = event.nativeEvent;
+    const offsetY = contentOffset.y;
+
+    if (offsetY <= 40) {
+      setFull(false);
+    } else if (offsetY >= 0) {
+      setFull(true);
+    }
+  };
 
   return (
     <Container>
@@ -19,9 +29,34 @@ const Home = () => {
         <HomeCard />
         <FinancialInfos />
       </Body>
-      <UserHistoric />
+
+      <ScrollView
+        onScroll={handleScroll}
+        style={full ? styles.fullScrollView : styles.partialScrollView}
+      >
+        {Array.from({ length: 20 }).map((_, index) => (
+          <List.Item key={index} title={`Item ${index + 1}`} />
+        ))}
+      </ScrollView>
     </Container>
   );
 };
 
 export default Home;
+
+const { height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  fullScrollView: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: height,
+    backgroundColor: "blue",
+  },
+  partialScrollView: {
+    height: 200,
+    backgroundColor: "red",
+  },
+});
