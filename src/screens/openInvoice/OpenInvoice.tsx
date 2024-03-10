675219930b98c754af632b7dc3b3ba4d59a7b4c7
GabrielTFernandes <devgabrieltrigueiro@gmail.com>
2024-03-10 07:55:13 -0300

    tela básica de pagar fatura

--- src/screens/openInvoice/OpenInvoice.tsx
+++ src/screens/openInvoice/OpenInvoice.tsx
@@ -1,12 +1,116 @@
+import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text } from "react-native";
+import { Dimensions, Text, View } from "react-native";
+import { LineChart } from "react-native-chart-kit";
 
 import { RootStackParamList } from "../../../App";
+import theme from "../../styles/theme";
+import {
+  Container,
+  HeaderIncoive,
+  InvoiceInfos,
+  InvoicePeriod,
+  LabelLine,
+  LabelValue,
+  LineContainer,
+  MiniCard,
+} from "./styles";
+import UserHeader from "../../components/userHeader/UserHeader";
+import DefaultButton from "../../components/defaultButton/DefaultButton";
 
 type Props = NativeStackScreenProps<RootStackParamList, "OpenInvoice">;
 
 const OpenInvoice = ({ navigation, route }: Props) => {
-  return <Text>OpenInvoice</Text>;
+  return (
+    <Container>
+      <UserHeader navigation={navigation} route={route} />
+      <HeaderIncoive>
+        <MiniCard
+          onPress={() => console.log("Pressed")}
+          rippleColor={"#fff"}
+          borderless
+        >
+          <></>
+        </MiniCard>
+        <InvoicePeriod>
+          <Text
+            style={{
+              color: theme.COLORS.BLUE5,
+              fontFamily: theme.FONTS.POPPINSMEDIUM,
+              fontSize: 18,
+            }}
+          >
+            Faturas
+          </Text>
+          <Text
+            style={{
+              color: theme.COLORS.GRAY3,
+              fontFamily: theme.FONTS.POPPINSMEDIUM,
+              fontSize: 12,
+            }}
+          >
+            Out - Fev
+          </Text>
+        </InvoicePeriod>
+      </HeaderIncoive>
+      <LineChart
+        data={{
+          labels: ["Jan", "Fer", "Mar", "Abr", "Mai", "Jun"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width - 40}
+        height={220}
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fff",
+          backgroundGradientTo: "#fff",
+          color: (opacity = 1) => theme.COLORS.BLUE,
+          labelColor: (opacity = 1) => theme.COLORS.BLUE,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#fff",
+          },
+        }}
+        bezier
+        style={{
+          borderRadius: 16,
+        }}
+      />
+      <InvoiceInfos>
+        <LineContainer>
+          <LabelLine>Vencimento</LabelLine>
+          <LabelValue>10 de Dezembro</LabelValue>
+        </LineContainer>
+        <LineContainer>
+          <LabelLine>Valor</LabelLine>
+          <LabelValue>R$ 1.000,00</LabelValue>
+        </LineContainer>
+        <LineContainer>
+          <LabelLine>Compras até</LabelLine>
+          <LabelValue>--</LabelValue>
+        </LineContainer>
+        <DefaultButton
+          label="Pagar fatura"
+          action={() => console.log("Pressed")}
+        />
+      </InvoiceInfos>
+    </Container>
+  );
 };
 
 export default OpenInvoice;
--- src/screens/openInvoice/styles.ts
+++ src/screens/openInvoice/styles.ts
@@ -1,4 +1,59 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { TouchableRipple } from "react-native-paper";
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components";
 
-export const Container = styled(View)``;
+export const Container = styled(View)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+`;
+
+export const HeaderIncoive = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin: ${RFValue(20)}px;
+`;
+
+export const MiniCard = styled(TouchableRipple)`
+  width: ${RFValue(150)}px;
+  height: ${RFValue(70)}px;
+  background-color: ${({ theme }) => theme.COLORS.BLUE};
+  border-radius: ${RFValue(50)}px;
+  margin-bottom: ${RFValue(20)}px;
+`;
+
+export const InvoicePeriod = styled(View)`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: flex-end;
+`;
+
+export const InvoiceInfos = styled(View)`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: self-start;
+  padding: ${RFValue(40)}px ${RFValue(20)}px;
+  gap: ${RFValue(30)}px;
+`;
+
+export const LineContainer = styled(View)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const LabelLine = styled(Text)`
+  color: ${({ theme }) => theme.COLORS.GRAY3};
+  font-family: ${({ theme }) => theme.FONTS.POPPINSMEDIUM};
+  font-size: ${RFValue(16)}px;
+`;
+
+export const LabelValue = styled(Text)`
+  color: ${({ theme }) => theme.COLORS.BLACK};
+  font-family: ${({ theme }) => theme.FONTS.POPPINSREGULAR};
+  font-size: ${RFValue(12)}px;
+`;
